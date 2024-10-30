5a07fbf897129c45a90c3d4fd155e234aa3c8bd9
salesni <71354362+salesni@users.noreply.github.com>
2024-10-30 09:23:23 -0600

    Solution JewelsAndStones.py

---
+++ HashMaps&Set/JewelsAndStones.py
@@ -0,0 +1,30 @@
+"""
+
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+ 
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+
+
+"""
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewels = set(jewels)
+        how_many_jewels = 0
+
+        for stone in stones:
+            if stone in jewels:
+                how_many_jewels += 1
+        
+        return how_many_jewels
