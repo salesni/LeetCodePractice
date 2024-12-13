2b493f540a37f8b51b918941ee96743f60e468d0
salesni <71354362+salesni@users.noreply.github.com>
2024-12-13 11:24:08 -0600

    Solution  MovePiecesObtainString.py

---
+++ TwoPointers/MovePiecesObtainString.py
@@ -0,0 +1,72 @@
+
+"""
+2337. Move Pieces to Obtain a String
+
+You are given two strings start and target, both of length n. Each string consists only of the characters 
+'L', 'R', and '_' where:
+
+The characters 'L' and 'R' represent pieces, where a piece 'L' can move to the left only if there is 
+a blank space directly to its left, and a piece 'R' can move to the right only if there is a blank 
+space directly to its right.
+The character '_' represents a blank space that can be occupied by any of the 'L' or 'R' pieces.
+Return true if it is possible to obtain the string target by moving the pieces of the string start
+any number of times. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: start = "_L__R__R_", target = "L______RR"
+Output: true
+Explanation: We can obtain the string target from start by doing the following moves:
+- Move the first piece one step to the left, start becomes equal to "L___R__R_".
+- Move the last piece one step to the right, start becomes equal to "L___R___R".
+- Move the second piece three steps to the right, start becomes equal to "L______RR".
+Since it is possible to get the string target from start, we return true.
+Example 2:
+
+Input: start = "R_L_", target = "__LR"
+Output: false
+Explanation: The 'R' piece in the string start can move one step to the right to obtain "_RL_".
+After that, no pieces can move anymore, so it is impossible to obtain the string target from start.
+Example 3:
+
+Input: start = "_R", target = "R_"
+Output: false
+Explanation: The piece in the string start can move only to the right, so it is impossible to obtain the string target from start.
+"""
+
+
+class Solution:
+    def canChange(self, start: str, target: str) -> bool:
+
+        start_index = 0
+        end_index = 0
+        length = len(start)
+
+        while start_index < length or end_index < length:
+
+            while end_index < length and target[end_index] == "_":
+                end_index += 1
+            while start_index < length and start[start_index] == "_":
+                start_index += 1
+
+            if start_index == length or end_index == length:
+                return start_index == end_index
+
+            if start[start_index] == target[end_index]:
+                if start[start_index] == "L":
+                    if end_index > start_index:
+                        return False
+                else:
+                    if end_index < start_index:
+                        return False
+            else:
+                return False
+            start_index += 1
+            end_index += 1
+
+        return True
+
+            
+
